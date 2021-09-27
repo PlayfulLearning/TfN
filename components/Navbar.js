488b7e7127da6a42897e0551e0ecf276a04c6b8f
PlayfulLearning <91479087+PlayfulLearning@users.noreply.github.com>
2021-09-27 10:21:36 -0400

    Initial commit

---
+++ components/Navbar.js
@@ -0,0 +1,25 @@
+// return HTML for navigation section
+export default function Navbar(page, items){
+    return `
+    <nav class="navbar">
+        <ul>
+           ${page==='project'? (
+                `<li class="nav-title">
+                    <a href="/fetlab/#projects">Future Educational Technology Lab</a>
+                </li>`
+           ):(
+            `<li>
+                <a href="#about">ABOUT</a>
+            </li>
+            <li>
+                <a href="#projects">PROJECTS</a>
+            </li>
+            <li>
+                <a href="#people">PEOPLE</a>
+            </li>
+            `
+            )}
+        </ul>
+    </nav>`
+}
+
